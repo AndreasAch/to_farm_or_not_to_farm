7216aa13de3694d4c602fe4e16cd532317366e17
AndreasAch <at.achilleos@gmail.com>
2023-10-11 12:52:51 +0200

    Heroku deployment attempt

--- src/app.py
+++ src/app.py
@@ -1,29 +1,32 @@
-import requests
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Allow CORS for all routes
 
-lobby_players = []  # List to store player information
+players = []
 
 @app.route('/')
 def hello():
     return 'Hello, Farm Desktop App!'
 
-@app.route('/lobby_players', methods=['GET'])
-def get_lobby_players():
-    return jsonify({'lobby_players': lobby_players})
+@app.route('/players')
+def get_players():
+    return jsonify({'players': [player['name'] for player in players]})
 
 @app.route('/join', methods=['POST'])
 def join_game():
     player_name = request.form['player_name']
     session_code = request.form['session_code']
 
-    # Add player to the list
-    lobby_players.append(player_name)
+    players.append({'name': player_name, 'session_code': session_code})
+
+    # Notify the PyQt5 app of the new player
+    send_player_info_to_pyqt(player_name, session_code)
 
     return jsonify({'message': 'Join request received.'})
+
+def send_player_info_to_pyqt(player_name, session_code):
+    # Implement this to notify the PyQt5 app
+    pass
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
