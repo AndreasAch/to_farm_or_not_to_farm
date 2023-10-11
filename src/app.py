6dc2ab5c2e4c6e5f5f67b57fc793d9c0c9e1a659
AndreasAch <at.achilleos@gmail.com>
2023-10-11 15:03:09 +0200

    Websocket fixes

--- src/app.py
+++ src/app.py
@@ -6,53 +6,9 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 players = set()
-
-@app.route('/')
-def hello():
-    return 'Hello, Farm Desktop App!'
-
-@app.route('/players')
-def get_players():
-    return jsonify({'players': list(players)})
-
-@app.route('/join', methods=['POST'])
-def join_game():
-    data = request.get_json()
-    player_name = data.get('player_name')
-    session_code = data.get('session_code')
-
-    # Add player to the set
-    players.add(player_name)
-
-    # Notify the desktop app about the player joining
-    notify_desktop_app(f'Player joined: {player_name}')
-
-    return jsonify({'message': 'Join request received.'})
-
-@app.route('/leave', methods=['POST'])
-def leave_game():
-    data = request.get_json()
-    player_name = data.get('player_name')
-
-    # Remove player from the set
-    players.remove(player_name)
-
-    # Notify the desktop app about the player leaving
-    notify_desktop_app(f'Player left: {player_name}')
-
-    return jsonify({'message': 'Leave request received.'})
-
-def notify_desktop_app(message):
-    asyncio.get_event_loop().run_until_complete(
-        send_message_to_desktop_app(message)
-    )
-
-async def send_message_to_desktop_app(message):
-    async with websockets.connect('ws://to-farm-or-not-tofarm.onrender.com/') as websocket:
-        await websocket.send(message)
+desktop_app_websocket = None
 
 def generate_session_code():
-    # Generate a random session code (e.g., 'ABCD123')
     return ''.join(random.choices('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890', k=6))
 
 @app.route('/session_code', methods=['GET'])
@@ -61,5 +17,17 @@ def get_session_code():
     return jsonify({'session_code': session_code})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    # ... existing code ...
+
+    async def websocket_handler(websocket, path):
+        global desktop_app_websocket
+        desktop_app_websocket = websocket
+        try:
+            while True:
+                await asyncio.sleep(10)  # Keep the WebSocket connection alive
+        except websockets.exceptions.ConnectionClosed:
+            pass
+
+    start_server = websockets.serve(websocket_handler, "0.0.0.0", 8765)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
--- src/main.py
+++ src/main.py
@@ -1,8 +1,63 @@
+# import sys
+# import asyncio
+# import requests
+# import websockets
+# from websockets import connect
+# from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+#
+# class FarmApp(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#         self.initUI()
+#
+#     def initUI(self):
+#         self.setWindowTitle('Farm Desktop App')
+#         self.setGeometry(300, 300, 400, 200)  # Adjust dimensions as needed
+#
+#         layout = QVBoxLayout()
+#
+#         self.label = QLabel('Farm Desktop App', self)
+#         layout.addWidget(self.label)
+#
+#         # Display the session code obtained from the server
+#         session_code = self.fetch_session_code()
+#         self.session_label = QLabel(f'Session Code: {session_code}', self)
+#         layout.addWidget(self.session_label)
+#
+#         self.setLayout(layout)
+#         self.show()
+#
+#         asyncio.get_event_loop().run_until_complete(self.websocket_handler())
+#
+#     def fetch_session_code(self):
+#         # Replace with your server endpoint to fetch the session code
+#         endpoint = 'https://to-farm-or-not-tofarm.onrender.com/session_code'
+#         try:
+#             response = requests.get(endpoint)
+#             response.raise_for_status()  # Raise an exception for bad status codes (4xx, 5xx)
+#             return response.json().get('session_code', 'Unknown')
+#         except requests.exceptions.RequestException as e:
+#             print('Error:', e)
+#             return 'Error fetching session code'
+#
+#     async def websocket_handler(self):
+#         uri = "wss://to-farm-or-not-tofarm.onrender.com/"  # Replace with your WebSocket URL
+#         async with websockets.connect(uri) as websocket:
+#             while True:
+#                 message = await websocket.recv()
+#                 self.session_label.setText(f"Session Code: {message}")
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     farm_app = FarmApp()
+#     sys.exit(app.exec_())
+
 import sys
-import asyncio
 import requests
-import websockets
+import asyncio
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtCore import Qt
+import websockets
 
 class FarmApp(QWidget):
     def __init__(self):
@@ -18,41 +73,25 @@ class FarmApp(QWidget):
         self.label = QLabel('Farm Desktop App', self)
         layout.addWidget(self.label)
 
-        # Display the session code obtained from the server
-        session_code = self.fetch_session_code()
-        self.session_label = QLabel(f'Session Code: {session_code}', self)
+        self.session_label = QLabel('Session Code: Loading...', self)
         layout.addWidget(self.session_label)
 
         self.setLayout(layout)
         self.show()
 
-    def fetch_session_code(self):
-        # Replace with your server endpoint to fetch the session code
-        endpoint = 'https://to-farm-or-not-tofarm.onrender.com/session_code'
-        try:
-            response = requests.get(endpoint)
-            response.raise_for_status()  # Raise an exception for bad status codes (4xx, 5xx)
-            return response.json().get('session_code', 'Unknown')
-        except requests.exceptions.RequestException as e:
-            print('Error:', e)
-            return 'Error fetching session code'
-
-    async def websocket_handler(self):
-        async with websockets.connect('wss://to-farm-or-not-tofarm.onrender.com/') as websocket:
-            while True:
-                message = await websocket.recv()
-                if message.startswith('Player joined: '):
-                    player_name = message[len('Player joined: '):]
-                    print('Player joined:', player_name)
-                    # Update your UI to reflect the new player
-
-                elif message.startswith('Player left: '):
-                    player_name = message[len('Player left: '):]
-                    print('Player left:', player_name)
-                    # Update your UI to reflect the player leaving
+    async def connect_to_server_and_get_session_code(self):
+        uri = "wss://your-websocket-url-here"  # Replace with your WebSocket URL
+        async with websockets.connect(uri) as websocket:
+            await websocket.send('get_session_code')  # Request session code
+            session_code = await websocket.recv()  # Receive session code
+            self.session_label.setText(f'Session Code: {session_code}')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     farm_app = FarmApp()
-    asyncio.create_task(farm_app.websocket_handler())  # Start WebSocket handling
+
+    # Run the asynchronous function to connect and get the session code
+    asyncio.create_task(farm_app.connect_to_server_and_get_session_code())
+
     sys.exit(app.exec_())
+
