189b36d5fc25c3ae9dbcff9732fde30d33fbab27
AndreasAch <at.achilleos@gmail.com>
2023-10-11 14:03:20 +0200

    Port changes

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import os
 from random import random
 
 from flask import Flask, request, jsonify
@@ -40,4 +41,5 @@ def generate_session_code():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
